290952f2935e2a5e225db4896fdcb3639bd36822
denial <danpan1232@gmail.com>
2025-08-31 03:36:08 -0400

    Minor page structure changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { LocationProvider, hydrate, prerender as ssr } from 'preact-iso';
 import { Router, Route, Switch } from "wouter-preact";
 import { Home } from './pages/Home/index.jsx';
-import { NotFound } from './pages/_404.jsx';
+import { NotFound } from './pages/404.js';
 import { Ideas } from './pages/Ideas/index.js';
 import './style.css';
 import './font.css';
