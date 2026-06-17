b0d50119262cdf6dd5cc018e5682bc3a74f88bf2
denial <danpan1232@gmail.com>
2026-06-17 13:28:08 -0400

    update homepage

--- src/pages/Home/content/contentHome.tsx
+++ src/pages/Home/content/contentHome.tsx
@@ -1,6 +1,8 @@
 import minedemo from '../../../assets/video/minedemov2.mp4';
 import s4shadowplay from '../../../assets/images/s4-shadowplay.png'
 import { setMeta } from '../../../utils/setmeta';
+import { Link } from 'wouter-preact';
+import leesinurf from '../../../assets/images/leesinURFpreview.png'
 
 export default function ContentHome() {
 
@@ -10,22 +12,38 @@ export default function ContentHome() {
 
         <>
             <div class="latest">
-                <div class="custom-header" onClick={() => window.open("https://github.com/denialpan/dansweeper-ml", "_blank", "noopener,noreferrer")} style={{ cursor: "pointer" }}>
-                    Latest Project - dansweeper
-                </div>
-                <video
-                    autoplay
-                    loop
-                    muted
-                    playsinline
-                    width="720"
-                    preload="metadata"
-                    class='latest-video'
-                >
-                    <source src={minedemo} type="video/webm" />
-                </video>
-                <div class="custom-caption custom-divider-bottom">
-                    minesweeper from scratch with multiple planned solving and generation algorithms
+                <Link class="custom-header project-link" href="/projects/leesinurf">
+                    Latest Project - complex video analysis and audio translation
+                </Link>
+                <div>
+                    <img src={leesinurf} alt="lee sin urf workflow" width={720} />
+                    <div class="custom-caption">
+                        a custom League of Legends video and audio tool pipeline to analyze sophisticated niche gameplay, and further translate casual Chinese commentary to English
+                        <ul>
+                            <li>
+                                <b>OpenCV</b> - frame region extraction and icon recognition within a video
+                            </li>
+                            <li>
+                                <b>Whisper large-v3</b> - language transcription
+                            </li>
+                            <li>
+                                <b>UVR</b> - vocal isolation in noisy contexts
+                            </li>
+                            <li>
+                                <b>Qwen2.5-VL 14B</b> - Chinese to English translation
+                            </li>
+                            <li>
+                                <b>Qwen2.5-VL 7B</b> - visual analysis of broad gameplay frames
+                            </li>
+                            <li>
+                                <b>Davinci Resolve</b> - multitrack video editing and SRT subtitle creation
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="custom-divider-bottom">
+
+                    </div>
                 </div>
 
                 <div class="custom-header" onClick={() => window.open("https://github.com/denialpan/s4-shadowplay", "_blank", "noopener,noreferrer")} style={{ cursor: "pointer" }}>
